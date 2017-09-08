6af5d4e0f5536e2302b807f36d8e38e4f48de560
Anita Graser <anitagraser@gmx.at>
2017-09-08 12:27:50 +0200

    Initial upload

---
+++ cluster_lines_kmeans_with_ideal_number_of_clusters.py
@@ -0,0 +1,162 @@
+##Edge bundling=group
+##input=vector
+##metric=string euclidean
+##target_cluster_size=number 0.1
+##kmeans_output=output vector
+
+import numpy as np
+import processing 
+from processing.tools.vector import VectorWriter
+from qgis.core import *
+from PyQt4.QtCore import *
+from sklearn.cluster import KMeans
+from datetime import datetime
+from math import sqrt 
+
+t_start = datetime.now()
+print '{0}: Clustering using KMeans'.format(t_start)
+
+layer = processing.getObject(input)
+provider = layer.dataProvider()
+fields = provider.fields()
+fields.append(QgsField('CLUSTER', QVariant.Int))
+fields.append(QgsField('CLUSTER_N', QVariant.Int))
+writer = VectorWriter(kmeans_output, None,fields, provider.geometryType(), layer.crs() )
+
+
+# Perform clustering 
+X = []
+for edge in processing.features(layer):
+    geom = edge.geometry()
+    azimuth = geom.vertexAt(0).azimuth(geom.vertexAt(1))/4
+    
+    # mix start and end points
+    X.append([geom.vertexAt(0).x(),geom.vertexAt(0).y()])
+    X.append([geom.vertexAt(1).x(),geom.vertexAt(1).y()])
+    
+    
+    # start and end point x/y
+    # X.append([geom.vertexAt(0).x(),geom.vertexAt(0).y(),
+    #               geom.vertexAt(1).x(),geom.vertexAt(1).y()])
+    #
+    # with azimuth               
+    #X.append([geom.vertexAt(0).x(),geom.vertexAt(0).y(),azimuth])    
+    #X.append([geom.centroid().asPoint().x(),
+    #                  geom.centroid().asPoint().y(),
+    #                  geom.length(),
+    #                  azimuth])
+#print X
+
+
+
+
+clusters = 2 # initial number of clusters
+mean = target_cluster_size * target_cluster_size * 100 # make sure it's big enough
+labels=[]
+
+print sqrt(mean)
+print target_cluster_size
+
+while sqrt(mean) > target_cluster_size: 
+    
+    print("Clusters: {0}".format(clusters))
+
+    db = KMeans(n_clusters=clusters).fit(X)
+    pt_labels = list(db.labels_)
+
+    # labels[0] = cluster of line0 start 
+    # labels[1] = cluster of line0 end
+    # labels[2] = cluster of line1 start
+    # labels[3] = cluster of line1 end
+    # ...
+
+    label_pairs=[]
+    for ptl in pt_labels:
+        try:
+            label_pairs[-1]
+        except IndexError:
+            label_pairs.append([])
+        if len(label_pairs[-1]) < 2:
+            label_pairs[-1].append(ptl)
+        else:
+            label_pairs[-1].sort() # to ignore line direction 
+            label_pairs.append([ptl])
+
+    unique_line_labels = []
+    labels = []
+    for pair in label_pairs:
+        if pair not in unique_line_labels:
+            unique_line_labels.append(pair)
+        pair_line_label = unique_line_labels.index(pair)
+        labels.append(pair_line_label)
+    
+    # Score cluster quality
+    # first approach: size of convex hull of start points or end points per cluster, respectively 
+    start_points = []
+    end_points = []
+    for l in range(0,max(labels)+1):
+        start_points.append(QgsMultiPointV2())
+        end_points.append(QgsMultiPointV2())
+
+    for i,inFeat in enumerate(processing.features(layer)):
+        geom = inFeat.geometry()
+        label = int(labels[i])
+        if label >= 0:
+            start_points[label].addGeometry(QgsPointV2(geom.vertexAt(0).x(),geom.vertexAt(0).y()))
+            end_points[label].addGeometry(QgsPointV2(geom.vertexAt(1).x(),geom.vertexAt(1).y()))
+     
+    start_areas = [QgsGeometry(pts).convexHull().area() for pts in start_points]
+    end_areas = [QgsGeometry(pts).convexHull().area() for pts in end_points]
+    sizes = start_areas  + end_areas
+    mean = sum(sizes) / len(sizes)
+
+    print("Mean area: {0} or approx {1}^2".format(mean, sqrt(mean) ))
+    print("Max area: {0} or approx {1}^2".format(max(sizes), sqrt(max(sizes)) ))
+
+    if sqrt(mean) > (10*target_cluster_size):
+        clusters += 2 
+    else:
+        clusters += 1
+    
+
+
+
+
+# Determine number of edges per cluster 
+cluster_sizes = []
+for l in range(0,max(labels)+1):
+    cluster_sizes.append(0)
+     
+for label in labels:
+    if label >= 0:
+        cluster_sizes[label] = cluster_sizes[label]+1
+
+#print labels
+#print max(labels)
+#print max(cluster_sizes)
+
+
+# Create output 
+outFeat = QgsFeature()
+for i,inFeat in enumerate(processing.features(layer)):
+    inGeom = inFeat.geometry()
+    outFeat.setGeometry(inGeom)
+    attrs = inFeat.attributes()
+    label = int(labels[i])
+    attrs.append(label)
+    size = 1
+    if label >= 0:
+        size = int(cluster_sizes[label])
+    attrs.append(size)
+    outFeat.setAttributes(attrs)
+    writer.addFeature(outFeat)
+
+del writer
+
+
+
+
+
+t_end = datetime.now()
+print('{0}: Finished!'.format(t_end))
+print('Run time: {0}'.format(t_end - t_start))
---
+++ summarize.py
@@ -0,0 +1,145 @@
+##Edge bundling=group
+##input_layer=vector
+##cluster_field=field input_layer
+##weight_field=field input_layer
+##use_weight_field=boolean true
+##max_distance=number 0.008
+##collapsed_lines=output vector
+
+from qgis.core import *
+from qgis.gui import *
+import qgis.utils
+from PyQt4.QtCore import *
+import processing
+from processing.tools.vector import VectorWriter
+from datetime import datetime 
+
+class Edge(QgsFeature):
+    def __init__(self, feature, weight=1):
+        super(Edge,self).__init__(feature)
+        if use_weight_field: 
+            self.weight = float(self.attributes()[weight_index]) #self[weight_field] 
+        else:
+            self.weight = weight 
+        self.agg_weight = self.weight
+    
+    def increase_weight(self,value=1):
+        self.agg_weight += value
+    
+    def get_weight(self):
+        return self.weight 
+        
+    def get_agg_weight(self):
+        return self.agg_weight
+        
+    
+class EdgeCluster():
+    def __init__(self,edges):
+        self.edges = edges       
+        self.index = QgsSpatialIndex()
+        for e in self.edges:
+            self.index.insertFeature(e)
+        self.allfeatures = {edge.id(): edge for (edge) in self.edges}
+    
+    def get_size(self):
+        return len(self.edges) 
+    
+    def collapse_lines(self):
+        ids_to_delete = []
+        for edge1 in self.edges:
+            geom1 = edge1.geometry()
+            # get other edges in the vicinty
+            tolerance = min(max_distance,geom1.length()/2)
+            ids = self.index.intersects(edge1.geometry().buffer(tolerance,4).boundingBox())
+            
+            for id in ids:
+                edge2 = self.allfeatures[id]            
+                if edge1.id()>edge2.id():
+                    geom2 = edge2.geometry()
+                    d0 = geom1.vertexAt(0).distance(geom2.vertexAt(0))
+                    d1 = geom1.vertexAt(1).distance(geom2.vertexAt(1))
+                    distance = d0 + d1
+                    if d0 <= (tolerance/2) and d1 <= (tolerance/2):
+                        edge1.increase_weight(edge2.get_weight())
+                        edge2.increase_weight(edge1.get_weight()) 
+        return ids_to_delete
+
+
+t_start = datetime.now()
+print '{0}: Collapsing lines'.format(t_start)
+
+layer = processing.getObject(input_layer)
+crs = layer.crs()
+provider = layer.dataProvider()
+fields = provider.fields()
+fields.append(QgsField('SEG_ID',QVariant.Int))
+fields.append(QgsField('MERGED_N', QVariant.Double))
+writer = processing.VectorWriter(collapsed_lines, None, fields, QGis.WKBLineString, crs)
+features = list(processing.features(layer))
+weight_index = provider.fieldNameIndex(weight_field)
+
+# get all labels from the input features 
+labels = []
+for feat in features:
+    labels.append(int(feat[cluster_field]))
+    
+# one cluster per label
+clusters = []
+for l in range(0,max(labels)+1):
+    clusters.append(list())
+
+# populate clusters
+vl = QgsVectorLayer("LineString", "line_segments", "memory")
+pr = vl.dataProvider()
+pr.addAttributes(fields)
+vl.updateFields()
+
+
+
+feature_id = 0 
+#all_segments = []
+for i,label in enumerate(labels):
+    attrs = features[i].attributes()
+    polyline = features[i].geometry().asPolyline()
+    fet = QgsFeature()
+    for j in range(0,len(polyline)-1):
+        g = QgsGeometry.fromPolyline([polyline[j],polyline[j+1]])
+        fet.setGeometry(g)
+        fet.setAttributes(attrs+[j])
+        fet.setFeatureId(feature_id)
+        feature_id += 1
+        edge = Edge(fet)
+        #all_segments.append(edge)
+        if label >= 0: 
+            clusters[label].append(edge)
+        else:
+            clusters.append(edge)
+            
+#pr.addFeatures(all_segments)
+
+            
+for i,cluster in enumerate(clusters):
+    clusters[i] = EdgeCluster(cluster)            
+       
+# collapse lines 
+ids_to_delete = []
+for i,cluster in enumerate(clusters):
+    print 'Cluster #{0} (size: {1})'.format(i,cluster.get_size())
+    ids_to_delete += cluster.collapse_lines()
+
+# create output 
+for cluster in clusters:
+    for g,edge in enumerate(cluster.edges):
+        #if edge.id() not in ids_to_delete:
+            fet = QgsFeature()
+            fet.setGeometry( edge.geometry() )
+            attrs = edge.attributes()
+            attrs.append(int(edge.get_agg_weight()))
+            fet.setAttributes( attrs )
+            writer.addFeature( fet )
+del writer                
+
+
+t_end = datetime.now()
+print '{0}: Finished!'.format(t_end)
+print 'Run time: {0}'.format(t_end - t_start)
