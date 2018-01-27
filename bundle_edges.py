71751bd49460e468f3bedd16bf4a58930caf2ea2
Anita Graser <anitagraser@gmx.at>
2018-01-27 20:04:24 +0100

    Cleanup

--- bundle_edges.py
+++ bundle_edges.py
@@ -64,7 +64,7 @@ class MiscUtils:
         v1 = line.vertexAt(1)
         length = max(line.length(), 10**(-6))
         r = ((v0.y() - pt.y()) * (v0.y() - v1.y()) - 
-			 (v0.x() - pt.x()) * (v1.x() - v0.x())) / (length**2)
+             (v0.x() - pt.x()) * (v1.x() - v0.x())) / (length**2)
         return QgsPoint(v0.x() + r * (v1.x() - v0.x()), v0.y() + r * (v1.y() - v0.y()))
 
 
@@ -74,20 +74,20 @@ class EdgeCluster():
     
     def __init__(self, edges):
         self.S = initial_step_size  # Weighting factor (needs to be cached, because will be decreased in every cycle)
-        self.I = iterations  		# Number of iterations per cycle (needs to be cached, because will be decreased in every cycle)
-        self.edges = edges			# Edges to bundle in this cluster
-        self.edge_lengths = []		# Array to cache edge lenghts
-        self.E = len(edges)			# Number of edges
-        self.EP = 2   				# Current number of edge points
-        self.SP = 0  				# Current number of subdivision points
+        self.I = iterations         # Number of iterations per cycle (needs to be cached, because will be decreased in every cycle)
+        self.edges = edges          # Edges to bundle in this cluster
+        self.edge_lengths = []      # Array to cache edge lenghts
+        self.E = len(edges)         # Number of edges
+        self.EP = 2                 # Current number of edge points
+        self.SP = 0                 # Current number of subdivision points
         self.compatibility_matrix = np.zeros(shape=(self.E,self.E)) # Compatibility matrix
-        self.direction_matrix = np.zeros(shape=(self.E,self.E))   	# Encodes direction of edge pairs
-        self.N = (2**cycles ) + 1   								# Maximum number of points per edge
-        self.epm_x = np.zeros(shape=(self.E,self.N))				# Bundles edges (x-values)
-        self.epm_y = np.zeros(shape=(self.E,self.N))				# Bundles edges (y-values)
+        self.direction_matrix = np.zeros(shape=(self.E,self.E))     # Encodes direction of edge pairs
+        self.N = (2**cycles ) + 1                                   # Maximum number of points per edge
+        self.epm_x = np.zeros(shape=(self.E,self.N))                # Bundles edges (x-values)
+        self.epm_y = np.zeros(shape=(self.E,self.N))                # Bundles edges (y-values)
         
     def compute_compatibilty_matrix(self):
-        """ Computing the edges compatibility matrix 
+        """
         Compatibility is stored in a matrix (rows = edges, columns = edges). 
         Every coordinate in the matrix tells whether the two edges (r,c)/(c,r) 
         are compatible, or not. The diagonal is always zero, and the other fields 
@@ -96,17 +96,17 @@ class EdgeCluster():
         """
         edges_as_geom = []
         edges_as_vect = []
-        for e_idx, edge in enumerate(self.edges) :
+        for e_idx, edge in enumerate(self.edges):
             geom = edge.geometry()
             edges_as_geom.append(geom)
             edges_as_vect.append(QgsVector(geom.vertexAt(1).x() - geom.vertexAt(0).x(), 
-										   geom.vertexAt(1).y() - geom.vertexAt(0).y()))
+                                           geom.vertexAt(1).y() - geom.vertexAt(0).y()))
             self.edge_lengths.append(edges_as_vect[e_idx].length())
             
         progress = 0
         
-        for i in range(self.E-1) :
-            for j in range(i+1, self.E) :
+        for i in range(self.E-1):
+            for j in range(i+1, self.E):
                 # Parameters 
                 lavg = (self.edge_lengths[i] + self.edge_lengths[j]) / 2.0
                 dot = edges_as_vect[i].normalized() * edges_as_vect[j].normalized()
@@ -116,8 +116,8 @@ class EdgeCluster():
                         
                 # Scale compatibility 
                 scale_comp = 2.0 / (lavg / min(self.edge_lengths[i], 
-									self.edge_lengths[j]) + max(self.edge_lengths[i], 
-									self.edge_lengths[j]) / lavg)
+                                    self.edge_lengths[j]) + max(self.edge_lengths[i], 
+                                    self.edge_lengths[j]) / lavg)
                         
                 # Position compatibility 
                 i0 = edges_as_geom[i].vertexAt(0)
@@ -137,17 +137,17 @@ class EdgeCluster():
                 I1 = MiscUtils.project_point_on_line(j1, edges_as_geom[i])
                 mid_I = QgsGeometry.fromPolyline([I0, I1]).centroid()
                 dist_I = I0.distance(I1)
-                if dist_I == 0.0 :
+                if dist_I == 0.0:
                     pass # previously: visibility = 0.0
-                else :
+                else:
                     visibility1 = max(0, 1 - ((2 * mid_E1.distance(mid_I)) / dist_I))
                 J0 = MiscUtils.project_point_on_line(i0, edges_as_geom[j])
                 J1 = MiscUtils.project_point_on_line(i1, edges_as_geom[j])
                 mid_J = QgsGeometry.fromPolyline([J0, J1]).centroid()
                 dist_J = J0.distance(J1)
-                if dist_J == 0.0 :
+                if dist_J == 0.0:
                     visibility2 = 0.0
-                else :
+                else:
                     visibility2 = max(0, 1 - ((2 * mid_E2.distance(mid_J)) / dist_J))
                 visibility_comp = min(visibility1, visibility2)
 
@@ -155,20 +155,20 @@ class EdgeCluster():
                 comp_score = angle_comp * scale_comp * pos_comp * visibility_comp
                     
                 # Fill values into the matrix (1 = yes, -1 = no) and use matrix symmetry (i/j = j/i) 
-                if comp_score >= compatibility :
+                if comp_score >= compatibility:
                     self.compatibility_matrix[i, j] = 1
                     self.compatibility_matrix[j, i] = 1
-                else :
+                else:
                     self.compatibility_matrix[i, j] = -1
                     self.compatibility_matrix[j, i] = -1
                     
                 # Store direction 
                 distStart1 = j0.distance(i0)
                 distStart2 = j1.distance(i0)
-                if distStart1 > distStart2 :
+                if distStart1 > distStart2:
                     self.direction_matrix[i, j] = -1
                     self.direction_matrix[j, i] = -1
-                else :
+                else:
                     self.direction_matrix[i, j] = 1
                     self.direction_matrix[j, i] = 1
     
@@ -179,7 +179,7 @@ class EdgeCluster():
         # Create compatibility matrix 
         self.compute_compatibilty_matrix()
         
-        for e_idx, edge in enumerate(self.edges) :
+        for e_idx, edge in enumerate(self.edges):
             vertices = edge.geometry().asPolyline()
             self.epm_x[e_idx, 0] = vertices[0].x()
             self.epm_y[e_idx, 0] = vertices[0].y()
@@ -187,22 +187,22 @@ class EdgeCluster():
             self.epm_y[e_idx, self.N-1] = vertices[1].y()
         
         # For each cycle
-        for c in range(cycles) :    
+        for c in range(cycles):
             print 'Cycle {0}'.format(c)
             # New number of subdivision points 
             current_num = self.EP
             currentindeces = []
-            for i in range(current_num) :
+            for i in range(current_num):
                 idx = int((float(i) / float(current_num - 1)) * float(self.N - 1))
                 currentindeces.append(idx)
             self.SP += 2 ** c
             self.EP = self.SP + 2
             edgeindeces = []
             newindeces = []
-            for i in range(self.EP) :
+            for i in range(self.EP):
                 idx = int((float(i) / float(self.EP - 1)) * float(self.N - 1))
                 edgeindeces.append(idx)
-                if idx not in currentindeces :
+                if idx not in currentindeces:
                     newindeces.append(idx)
             pointindeces = edgeindeces[1:self.EP-1]
 
@@ -210,16 +210,16 @@ class EdgeCluster():
             for idx in newindeces:
                 i = int((float(idx) / float(self.N - 1)) * float(self.EP - 1))
                 left = i - 1
-                leftidx = int(( float(left) / float(self.EP - 1) ) * float(self.N - 1))
+                leftidx = int((float(left) / float(self.EP - 1)) * float(self.N - 1))
                 right = i + 1
-                rightidx = int(( float(right) / float(self.EP - 1) ) * float(self.N - 1))
+                rightidx = int((float(right) / float(self.EP - 1)) * float(self.N - 1))
                 self.epm_x[:, idx] = ( self.epm_x[:, leftidx] + self.epm_x[:, rightidx] ) / 2.0
                 self.epm_y[:, idx] = ( self.epm_y[:, leftidx] + self.epm_y[:, rightidx] ) / 2.0
             
             # Needed for spring forces 
             KP0 = np.zeros(shape=(self.E,1))
             KP0[:,0] = np.asarray(self.edge_lengths)
-            KP = K / (KP0 * (self.EP - 1) )
+            KP = K / (KP0 * (self.EP - 1))
 
             # For all iterations (number decreased in every cycle) 
             for iteration in range(self.I):
@@ -237,12 +237,12 @@ class EdgeCluster():
                 electrostaticforces_x = np.zeros(shape=(self.E, self.SP))
                 electrostaticforces_y = np.zeros(shape=(self.E, self.SP))
                 # Loop through all edges 
-                for e_idx, edge in enumerate(self.edges) :
+                for e_idx, edge in enumerate(self.edges):
                     # Loop through compatible edges 
                     comp_list = np.where(self.compatibility_matrix[:,e_idx] > 0)
-                    for other_idx in np.nditer(comp_list, ['zerosize_ok']) :
+                    for other_idx in np.nditer(comp_list, ['zerosize_ok']):
                         otherindeces = pointindeces[:]
-                        if self.direction_matrix[e_idx,other_idx] < 0 :
+                        if self.direction_matrix[e_idx,other_idx] < 0:
                             otherindeces.reverse()
                         # Distance between points 
                         subtr_x = self.epm_x[other_idx, otherindeces] - self.epm_x[e_idx, pointindeces]
@@ -266,9 +266,9 @@ class EdgeCluster():
             self.S = self.S * sdc              # Decrease weighting factor
             self.I = int(round(self.I * idc))  # Decrease iterations
  
-        for e_idx in range(self.E) :
+        for e_idx in range(self.E):
             # Create a new polyline out of the line array 
-            line = map(lambda p,q : QgsPoint(p,q), self.epm_x[e_idx], self.epm_y[e_idx] ) 
+            line = map(lambda p,q: QgsPoint(p,q), self.epm_x[e_idx], self.epm_y[e_idx]) 
             self.edges[e_idx].setGeometry(QgsGeometry.fromPolyline(line))
 
 
@@ -289,24 +289,24 @@ eps = 0.000001
 
 # Create edge list
 edges = []
-for feature in vlayer.getFeatures() :
+for feature in vlayer.getFeatures():
     edges.append(feature)
 
 # Create clusters
 clusters = []
-if use_clustering_result == True :
+if use_clustering_result == True:
     # Arrange edges in clusters according to cluster-id
     labels = []
     for edge in edges :
         labels.append(edge[cluster_field])
-    for l in range(0, max(labels)+1) :
+    for l in range(0, max(labels) + 1):
         clusters.append(list())
-    for i,label in enumerate(labels) :
+    for i,label in enumerate(labels):
         if label >= 0 : 
             clusters[label].append(edges[i])
         else :
             clusters.append([edges[i]])
-    for i,cluster in enumerate(clusters) :
+    for i,cluster in enumerate(clusters):
         clusters[i] = EdgeCluster(cluster)
 else :
     # If clustering should not be used, create only one big cluster containing all edges
@@ -317,7 +317,7 @@ else :
     clusters = [EdgeCluster(edges)]
 
 # Do edge-bundling (separately for all clusters)
-for c,cl in enumerate(clusters) :
+for c,cl in enumerate(clusters):
     if cl.E > 1 :
         cl.force_directed_eb()
     progress.setPercentage(10 + 80 * (c / len(clusters)))
@@ -327,15 +327,15 @@ progress.setPercentage(90)
 progress.setText('Create output...')
 writer = processing.VectorWriter(bundled_edges, None, fields, QGis.WKBLineString, vlayer.crs())
 for cl in clusters:
-    for e,edge in enumerate(cl.edges) :
+    for e,edge in enumerate(cl.edges):
         feat = QgsFeature()
         feat.setGeometry(edge.geometry())
-        if use_clustering_result == False :
+        if use_clustering_result == False:
             attr = edge.attributes()
             attr.append(1)
             attr.append(len(edges))
             feat.setAttributes(attr)
-        else :
+        else:
             feat.setAttributes(edge.attributes())
         writer.addFeature(feat)
 del writer
